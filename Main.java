3a91234d68cf258e29a0b60bf6db1b6714b398d7
Ethan <ethan@knockturnal.io>
2026-05-17 11:21:50 +0200

    Added Name tracker

--- Main.java
+++ Main.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         
+        Scanner scanner = new Scanner(System.in);
+
+        String name = "";
+
         System.out.println("Hello World");
 
+        System.out.print("Enter Name: ");
+        name = scanner.nextLine();
+
+        System.out.println("Hello " + name);
     }
 }
